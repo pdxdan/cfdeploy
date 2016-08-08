c405dd26ccaa7f722ccb6a7ff8425793b227262b
Daniel Anolik <dan@anolik.net>
2016-08-07 22:59:25 -0700

    converted all looping to waiters. also direct stack lookup is faster for environments with many stacks.

--- deploy_or_update_stack.py
+++ deploy_or_update_stack.py
@@ -1,36 +1,83 @@
-## Status: creates new stacks fine, but fails to update existing stacks
+## Status: Creates and updates for most situations. Waiters seem to be working.
+
+# To do: handle errors where CF template has not changed
+
 
 import boto3
 import argparse
 import os
-# import json
-# import botocore.exceptions
+import botocore.exceptions
 
 parser = argparse.ArgumentParser()
 cfn_stack_params = []
 
 cftemplate = "CFTemplateSamples/HelloBucket.template"
-# cftemplate = "AWSCloudFormation-samples/LAMP_Single_Instance.template"
 cftemplatecontent = open(cftemplate, 'r').read()
 cf = boto3.client('cloudformation', region_name='us-west-2')
 
 def main():
     # print "The CF template looks like this: \n" + cftemplatecontent
-    # create_or_update_stack("LAMP-Single-Instance")
-    # create_or_update_stack("HelloBucket3")
-    print cf.waiter_names
+    create_or_update_stack("HelloBucket3")
+
+    # print cf.waiter_names
+    # stack_exists('HelloBucket3')
+
+    # print cf.describe_stacks(StackName='HelloBucket33')
+
+def create_or_update_stack(sn):
+    print "Creating or updating stack: " + sn
+    if stack_exists(sn):
+        status=cf.describe_stacks(StackName=sn)['Stacks'][0]['StackStatus']
+        debug("Stack status = " + status)
+        if status.endswith('COMPLETE'):
+            update_stack(sn)
+        elif status == 'CREATE_IN_PROGRESS':
+            cf.get_waiter('stack_create_complete').wait(StackName=sn)
+            update_stack(sn)
+        elif status == 'ROLLBACK_IN_PROGRESS':
+            cf.get_waiter('stack_create_complete').wait(StackName=sn)
+            create_stack(sn)
+        elif status.startswith('UPDATE') and status.endswith('PROGRESS'):
+            # Handles UPDATE_IN_PROGRESS, UPDATE_COMPLETE_CLEANUP_IN_PROGRESS, UPDATE_ROLLBACK_IN_PROGRESS & UPDATE_ROLLBACK_COMPLETE_CLEANUP_IN_PROGRESS
+            cf.get_waiter('stack_update_complete').wait(StackName=sn)
+            update_stack(sn)
+        elif status == 'DELETE_IN_PROGRESS':
+            cf.get_waiter('stack_delete_complete').wait(StackName=sn)
+            create_stack(sn)
+    else:
+        print("Stack name " + sn + " does not exist, so I'll create it")
+        create_stack(sn)
+
+
+def stack_exists(sn):
+    exists = False
+    try:
+        cf.describe_stacks(StackName=sn)
+    except botocore.exceptions.ClientError as e:
+        # AWS returns ValidationError when the stack doesn't exist
+        if str(e.response['Error']['Code']) == "ValidationError":
+            print "stack name " + sn + " does not exist"
+            debug("It looks like the stack does not exist because we received this error from AWS:\n  " + str(e))
+            exists = False
+        else:
+            # Some other unexpected error happened
+            raise e
+    else:
+        exists = True
+        print "stack name " + sn + " exists"
+    return exists
 
 
 def create_stack(sn):
+    debug("attempting to create stack: " + sn)
     cf.create_stack(
         StackName=sn,
         TemplateBody=cftemplatecontent,
         Parameters=cfn_stack_params,
         Capabilities=['CAPABILITY_IAM'],
-
-        #??? Open question: what behavior do I really want here?
-        DisableRollback=True
-    )
+        #??? Open question: what Rollback behavior do I really want here?
+        DisableRollback=True )
+    cf.get_waiter('stack_create_complete').wait(StackName=sn)
 
 
 def update_stack(sn):
@@ -39,54 +86,11 @@ def update_stack(sn):
         StackName=sn,
         TemplateBody=cftemplatecontent,
         Parameters=cfn_stack_params,
-        Capabilities=['CAPABILITY_IAM']
-    )
+        Capabilities=['CAPABILITY_IAM'] )
+    # waiter = cf.get_waiter('stack_update_complete')
+    # waiter.wait(StackName=sn)
+    cf.get_waiter('stack_update_complete').wait(StackName=sn)
 
-
-def create_or_update_stack(sn):
-    print "Create or update stack: " + sn
-    stacks = cf.list_stacks(
-        StackStatusFilter=[
-            'CREATE_COMPLETE', 'UPDATE_COMPLETE', 'UPDATE_ROLLBACK_COMPLETE', 'UPDATE_ROLLBACK_IN_PROGRESS',
-            'UPDATE_ROLLBACK_COMPLETE_CLEANUP_IN_PROGRESS', 'UPDATE_COMPLETE_CLEANUP_IN_PROGRESS',
-            'UPDATE_IN_PROGRESS'
-        ]
-    )
-    stack_exists = False
-    for s in stacks['StackSummaries']:
-        debug("Comparing against existing stack: " + str(s['StackName']))
-        if s['StackName'] == sn:
-            if s['StackStatus'].endswith("COMPLETE"):
-                print("Stack already exists, updating")
-                update_stack(sn)
-                stack_exists = True
-                break
-            else:
-                stack_ready = False
-                total_time = 0
-                while stack_ready is False:
-                    print(
-                        "Stack exists but in state, {0} "
-                        "and not ready to be updated, so I'll wait".format(s['StackStatus'])
-                    )
-                    stack_info = cf.describe_stacks(
-                        StackName=sn
-                    )['Stacks'][0]
-                    if stack_info['StackStatus'].endswith("COMPLETE"):
-                        update_stack(sn)
-                        stack_ready = True
-                    elif stack_info['StackStatus'].endswith("FAILED"):
-                        raise Exception("Stack creation/update/rollback failed")
-                    else:
-                        total_time += 10
-                        if total_time > 3600:
-                            raise Exception("Stack creation/update timed out")
-                        else:
-                            time.sleep(10)
-                            continue
-    if stack_exists is False:
-        print("Stack name " + sn + " does not exist, so I'll create a new one")
-        create_stack(sn)
 
 def debug(s):
     # set HSQDEBUG=true in your environment to enable verbose logging
